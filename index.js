362c21e44b769649ec750babc3c66723d958d8a2
Sean Leather <sean.leather@gmail.com>
2020-06-18 08:42:03 +0200

    Put configuration in .eslintrc.json
    * Use `standardx` to test
    * Add `test.js` for testing against
    * Remove all unnecessary dependencies

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-module.exports = {
-  extends: 'standard',
-  rules: {
-    'space-before-function-paren': [
-      'error',
-      {
-        anonymous: 'always',
-        named: 'never',
-        asyncArrow: 'always',
-      },
-    ],
-    'comma-dangle': [
-      'warn',
-      'always-multiline',
-    ],
-  },
-}
+// We could put the ESLint configuration directly in this file. But by putting
+// it in a standardized external file, we can also use it to test this package.
+module.exports = require('./.eslintrc.json')
---
+++ test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+// This file is used to test with `standardx` and `./.eslintrc.json`. Therefore,
+// we ignore the fact that none of the variables are used.
+
+const commaDangle = {
+  a: 'a',
+}
+
+function noSpaceBeforeFunctionParen() {
+}
+
+const spaceBeforeFunctionParen = function () {
+}
